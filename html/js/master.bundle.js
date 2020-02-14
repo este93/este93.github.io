ae8de5997588f95010b98d6bd6545c2026c372a6
Stefan Petkovic <stefan.petkovic@diwanee.com>
2020-02-14 17:01:16 +0100

    Temperatures added

--- html/js/master.bundle.js
+++ html/js/master.bundle.js
@@ -160,16 +160,55 @@ var lineGeneratorDashed = d3.line().x(function (d) {
 	return yScale(dashValue(d));
 }).curve(d3.curveCardinal);
 
-// var defs = svg.append("defs");
-// var filter = defs.append("filter")
-//     .attr("id", "places-blur")
-//     .attr("height", "150%");
-// filter.append("feGaussianBlur")
-//     .attr("in", "SourceAlpha")
-//     .attr("stdDeviation", 50)
-//     .attr("result", "blur");
+var svgDefs = svg.append('defs');
+var mainGradient = svgDefs.append('linearGradient').attr('id', 'mainGradient');
+
+mainGradient.append('stop').attr('class', 'stop-right')
+// .attr('stop-opacity', 0.5)
+.attr('offset', '0%');
+
+mainGradient.append('stop').attr('class', 'stop-left').attr('offset', '10%');
+
+mainGradient.append('stop').attr('class', 'stop-left').attr('offset', '20%');
+
+mainGradient.append('stop').attr('class', 'stop-right').attr('offset', '30%');
+
+mainGradient.append('stop').attr('class', 'stop-right').attr('offset', '40%');
+
+mainGradient.append('stop').attr('class', 'stop-left').attr('offset', '52%');
+
+mainGradient.append('stop').attr('class', 'stop-left').attr('offset', '64%');
+
+mainGradient.append('stop').attr('class', 'stop-right').attr('offset', '76%');
+
+mainGradient.append('stop').attr('class', 'stop-left').attr('offset', '88%');
+
+mainGradient.append('stop').attr('class', 'stop-right').attr('offset', '100%');
+
+svg.append('rect').classed('filled', true).attr('x', 15).attr('y', 40).attr("filter", "url(#blur)").attr('height', innerHeight).attr('width', innerWidth);
+
+var svgDefs2 = svg.append('defs');
+var mainGradient2 = svgDefs2.append('linearGradient').attr('id', 'mainGradient2');
+mainGradient2.append('stop').attr('class', 'stop-right2').attr('offset', '0');
+mainGradient2.append('stop').attr('class', 'stop-left2').attr('offset', '5%');
+svg.append('rect').classed('filled2', true).attr('x', 0).attr('y', 0).attr('height', innerHeight + 100).attr('width', innerWidth + 100).transition().duration(3800).ease(d3.easeQuadInOut).attr('transform', 'translate(1000, 0)');
+
+var filter = svgDefs.append("filter").attr("id", "blur").append("feGaussianBlur").attr("stdDeviation", 7);
+
+// svg.append('rect')
+//     .attr('x', 0)
+//     .attr('y', 0)
+//     .attr('height', innerHeight + 100)
+//     .attr('width', innerWidth + 100)
+//     .attr('fill', '#333333')
+//     .transition()
+//         .duration(4000)
+// 		.attr('transform', 'translate(1000, 0)');
 
 var parseDate = d3.timeParse("%m/%d/%Y");
+
+svg.append('rect').attr('fill', '#333333').attr('class', 'topLeftMask').attr('x', 0).attr('y', 0).attr('height', 50).attr('width', 50);
+svg.append('rect').attr('fill', '#333333').attr('class', 'topRightMask').attr('x', innerWidth).attr('y', 0).attr('height', 50).attr('width', 50);
 
 var render = function render(data) {
 	xScale.domain(d3.extent(data, mainValue)).nice(data.length);
@@ -179,6 +218,14 @@ var render = function render(data) {
 	}), d3.max(data, function (d) {
 		return Math.max(yValue(d), dashValue(d));
 	})]);
+
+	var area = d3.area().x(function (d) {
+		return xScale(d.month) + 30;
+	}).curve(d3.curveNatural).y0(15).y1(function (d) {
+		return yScale(d.percent) + 20;
+	});
+
+	svg.append("path").data([data]).attr("class", "area").attr('fill', '#333333').attr("d", area);
 
 	var g = svg.append('g').attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
 
@@ -191,58 +238,41 @@ var render = function render(data) {
 	// const yAxisG = g.append('g').call(yAxis);
 	// yAxisG.selectAll('.domain').remove();
 
+
 	var path = g.append('path').attr('class', 'line-path').attr("ry", 20).attr("rx", 20).attr('d', lineGenerator(data));
 
 	var totalLength = path.node().getTotalLength();
 
-	path.attr("stroke-dasharray", totalLength + " " + totalLength).attr("stroke-dashoffset", totalLength).transition().duration(5000).ease(d3.easeQuadInOut).attr("stroke-dashoffset", 0);
+	path.attr("stroke-dasharray", totalLength + " " + totalLength).attr("stroke-dashoffset", totalLength).transition().duration(3000).ease(d3.easeQuadInOut).attr("stroke-dashoffset", 0);
 
 	var lineGoal = svg.append('g').attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')').attr('class', 'lineGoal');
-	lineGoal.append('line').style("stroke", 'black').attr('x1', 0).attr('y1', yScale(89.6)).attr('y2', yScale(89.6)).transition().duration(1000).attr('x2', innerWidth - 15);
+	lineGoal.append('line').attr('x1', 0).attr('y1', yScale(89.6)).attr('y2', yScale(89.6)).transition().duration(1000).attr('x2', innerWidth - 15);
 	lineGoal.append('text').attr('x', innerWidth - 10).attr('y', yScale(89.6) + 2).text('89,6%').attr('opacity', 0).transition().duration(500).delay(700).attr('opacity', 1);
 
 	g.selectAll("rect").data(data).enter().append("rect").attr('width', 2).attr('y', function (d) {
 		return yScale(yValue(d));
 	}).attr('x', function (d) {
 		return xScale(mainValue(d));
-	}).transition().delay(function (d, i) {
-		return i * 400;
+	}).attr('class', 'lineDropping').transition().delay(function (d, i) {
+		return i * 200;
 	}).duration(1000).ease(d3.easeSin).attr('height', function (d) {
 		return innerHeight - yScale(yValue(d));
 	});
 
-	// const blured = svg.append('g')
-	// 	.attr('transform', `translate(${margin.left}, ${margin.top})`);
-	//    blured.selectAll("rect")
-	// 	.data(data)
-	// 	.enter().append("rect")
-	//      	.style("filter","url(#places-blur)")
-	// 	.attr('width', 64)
-	// 	.attr('y', d => yScale(yValue(d)))
-	// 	.attr('x', d => xScale(mainValue(d)))
-	// 	.attr('height', d => innerHeight - yScale(yValue(d)))
-	// 	.attr('opacity', 0)
-	//     .transition()
-	//     	.delay(function(d, i) { return i * 400; })
-	//     	.duration(1000)
-	// 		.attr('opacity', 1)
-	//        	.ease(d3.easeSin)
-
-
 	var dots = svg.append('g').attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
 	dots.selectAll("dot").data(data).enter().append("circle").attr("r", 5).attr('opacity', 0).attr('cx', function (d) {
 		return xScale(mainValue(d));
-	}).transition().delay(function (d, i) {
-		return i * 400;
+	}).attr('class', 'circles').transition().delay(function (d, i) {
+		return i * 200;
 	}).duration(1000).ease(d3.easeSin).attr('opacity', 1).attr('cy', innerHeight);
 
-	var xAxisPercent = svg.append('g').attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
+	var xAxisPercent = svg.append('g').attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')').attr('class', 'percents');
 	xAxisPercent.selectAll("text").data(data).enter().append("text").attr('x', function (d) {
-		return xScale(mainValue(d)) - 10;
+		return xScale(mainValue(d)) - 17;
 	}).attr('opacity', 0).text(function (d) {
 		return d.percent + '%';
 	}).transition().delay(function (d, i) {
-		return i * 400;
+		return i * 200;
 	}).duration(1000).ease(d3.easeSin).attr('opacity', 1).attr('y', innerHeight + 20);
 
 	if (data[0].dashed) {
@@ -264,7 +294,7 @@ var render = function render(data) {
 
 		var dashArray = newDashes + " 0, " + totalLength;
 
-		path.attr("stroke-dashoffset", totalLength).attr("stroke-dasharray", dashArray).transition().duration(3000).attr("stroke-dashoffset", 0);
+		path.attr("stroke-dashoffset", totalLength).attr("stroke-dasharray", dashArray).transition().duration(2000).attr("stroke-dashoffset", 0);
 	}
 };
 
